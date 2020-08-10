e3e483dd0800d294721845a137caa3b24311cad5
Stefon Myrick <stefon.myrick@afcent.af.mil>
2020-08-10 15:18:03 -0400

    Revert "Revert "Fixed Nav links""
    This reverts commit 5338848a228009c4cab5841e18a6794f809f38af.

--- DemoApp/src/Components/Navbar.jsx
+++ DemoApp/src/Components/Navbar.jsx
@@ -3,24 +3,25 @@ import React from "react";
 function Navbar()
 {
 return(
+    <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" >Demo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand">Demo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse nabar-collapse" id="navbarText">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link">Input Msn<span class="sr-only"></span></a>
+                    <a class="nav-link" href="">Input Msn<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link">Misions</a>
+                    <a class="nav-link" href="">Misisons</a>
                 </li>
             </ul>
             
         </div>
     </nav>
-    
+    </div>
 );
 }
 export default Navbar;
