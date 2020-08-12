8fb6fba0439ba0e2eb117ea5cf5a0793593a7d25
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-08-12 09:36:32 -0400

    Change server.js to run in production

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('./DemoApp/build/'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
