fc17a2112278a683767658c39231abb47b92c34e
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-09-11 11:22:23 -0400

    Adds basename to browser router

--- DemoApp/src/index.js
+++ DemoApp/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/mm">
         <App />
     </BrowserRouter>,
     document.getElementById("root"));
