5940bef5d5aa42cc3e15fa4e10cbb5906dd6aebf
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-09-11 14:55:20 -0400

    Test changes to paths to fix issue

--- DemoApp/src/Components/Navbar.jsx
+++ DemoApp/src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
         <Router>
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand"><img src="../Images/Brand2.png" alt="brand" width="120px" height="30px"></img></a>
+                    <a className="navbar-brand"><img src="../mm/Images/Brand2.png" alt="brand" width="120px" height="30px"></img></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
