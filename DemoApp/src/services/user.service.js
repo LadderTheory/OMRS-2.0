81644a9fcfb68c07a2b41bb21653c8342a3e61ef
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-09-23 13:02:19 -0400

    tests auth functionality on k8s

--- DemoApp/src/services/user.service.js
+++ DemoApp/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'mm.sst.com/private/';
+const API_URL = 'http://mm.sst.com/private/';
 
 class UserService {
   getPublicContent() {
