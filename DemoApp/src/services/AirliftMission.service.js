18b62c36dbfa20224bf8510f915f74bcf15641d3
Stefon Myrick <stefon.myrick@afcent.af.mil>
2020-09-30 16:10:11 -0400

    Created new Data Service for new parameters

---
+++ DemoApp/src/services/AirliftMission.service.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://mm.sst.com/private/';
+
+class AirliftMissionService{
+    getAll() {
+        // return http.get("/parameters");
+        return axios.get(API_URL + `datamg`, { headers: authHeader() });
+      }
+      get(id) {
+        // return http.get(`/parameters/${id}`);
+        return axios.get(API_URL + `datamg/findBy/${id}`, { headers: authHeader() });
+      }
+      create(data) {
+        // return http.post("/parameters", data);
+        return axios.post(API_URL + `datamg`, data, { headers: authHeader() });
+      }
+      update(id, data) {
+        // return http.patch(`/parameters/${id}`, data);
+        return axios.patch(API_URL + `datamg/${id}`, data, { headers: authHeader() });
+      }
+      delete(id) {
+        // return http.delete(`/parameters/${id}`);
+        return axios.delete(API_URL + `datamg/${id}`, { headers: authHeader() });
+      }
+      deleteAll() {
+        // return http.delete(`/parameters`);
+        return axios.delete(API_URL + `datamg`, { headers: authHeader() });
+      }
+      retrieveSquadrons(){
+        // return http.get("/parameters/squadron");
+        return axios.get(API_URL + `datamg/squadrons`, { headers: authHeader() });
+      }
+      retrieveMsnTypes(){
+        return axios.get(API_URL + `datamg/msntypes`, { headers: authHeader() });
+      }
+      retrieveOperations(){
+        return axios.get(API_URL + `datamg/operations`, { headers: authHeader() });
+      }
+      retrieveLegTypes(){
+        return axios.get(API_URL + `datamg/legtypes`, { headers: authHeader() });
+      }
+      retrieveChannels(){
+        return axios.get(API_URL + `datamg/channels`, { headers: authHeader() });
+      }
+      retrieveBases(){
+        return axios.get(API_URL + `datamg/bases`, { headers: authHeader() });
+      }
+      retrieveAircraft(){
+        return axios.get(API_URL + `datamg/aircraft`, { headers: authHeader() });
+      }
+      retrieveCommTypes(){
+        return axios.get(API_URL + `datamg/commtypes`, { headers: authHeader() });
+      }
+      retrieveICAO(){
+        return axios.get(API_URL + `datamg/icao`, { headers: authHeader() });
+      }
+
+}
+
+export default new AirliftMissionService();
