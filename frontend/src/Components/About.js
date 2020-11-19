fc6ce862fd40fb01eddb9cd8e2848c3a72ad62dc
zachary.goyer <zachary.goyer@afcent.af.mil>
2020-11-19 14:34:42 -0500

    This wraps up some formatting changes to the About page and the comments for the same code.

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -10,32 +10,41 @@ import AboutViewFeedback from './AboutFiles/AboutViewFeedback';
 //About page component
 function AboutPage (props) {
 
+//this const sets the rendered text which will appear in the text area.
 const [renderedAboutText, setRenderedAboutText] = useState("");
 
+//each of these functions changes which text is displayed in the Text Area
 const setCreateMission = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutCreateMission);
 }
 const setAboutMissionReports = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutMissionReports);
 }
 
 const setAboutViewMission = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutViewMission);
 }
 
 const setAboutDataManagement = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutDataManagement);
 }
 
 const setAboutUserManagement = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutUserManagement);
 }
 
 const setAboutViewFeedback = (e) => {
+    setRenderedAboutText();
     setRenderedAboutText(AboutViewFeedback);
 }
-
+    //this renders the components.
     return(
+        // this div creates the vertical navigation bar along the left side of the component, and houses the text area that will display the selected text file.
         <div className="container">            
             <div className="card p-0 bg-dark align-items-center">
                 <div class="container-fluid h-100">
@@ -44,7 +53,7 @@ const setAboutViewFeedback = (e) => {
                             <nav class="navbar navbar-expand navbar-dark bg-dark flex-md-column flex-row align-items-start">
                                 <div class="collapse navbar-collapse">
                                     <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between navbar-dark">
-                                    {/* Change <a> to links or buttons */}
+                                    {/* These are all the current items listed in the About page, and this list will, presumably, expand as more features are added in future updates. */}
                                         <li><button class="dm" href="#" onClick={setCreateMission}>Creating Missions</button></li>
                                         <li><button class="dm" href="#" onClick={setAboutViewMission}>Viewing Missions</button></li>
                                         <li><button class="dm" href="#" onClick={setAboutMissionReports}>Mission Reports</button></li>   
@@ -56,7 +65,8 @@ const setAboutViewFeedback = (e) => {
                             </nav>
                         </aside>
                         <main class="col">
-                            <textarea className="form-control rounded-1" id="aboutTextArea" rows="15" placeholder="Please select an option from the side menu." value={renderedAboutText}></textarea>
+                            {/* this area is where the text area is rendered that will display the selected text. The text itself is rendered in the "renderedAboutText" value, and there is a placeholder line to direct the user to select a file to read. */}
+                            <textarea className="form-control rounded-1" id="aboutTextArea" rows="15" readOnly placeholder="Please select an option from the side menu." value={renderedAboutText}></textarea>
                         </main>
                     </div>
                 </div>
@@ -64,5 +74,5 @@ const setAboutViewFeedback = (e) => {
         </div>          
     )  
 }
-
+//this line exports the page.
 export default AboutPage;
