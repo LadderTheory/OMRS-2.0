562daa9aa9c032f90a2a88fc452fc41aa309e517
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-11-19 11:23:08 -0500

    Test matter most notification

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-//routes 9
+//routes 10
 require("./server/routes/auth.routes")(app);
 require("./server/routes/private.routes")(app);
 
