ea92aa4b6048eaa92cd9db7b2f06a2c712852d6f
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-11-19 15:25:53 -0500

    tries a different method of testing in a node image

--- frontend/sonar-project.js
+++ frontend/sonar-project.js
@@ -4,12 +4,11 @@ sonarqubeScanner(
     serverUrl: "http://192.168.1.78:31264",
     token: "d7fd8bcbe8fb344280999a6af10489c02e43c391",
     options: {
+      "sonar.projectkey": "omrs-prototype"
       "sonar.sources": "./src",
-      "sonar.exclusions": "**/tests/**",
-      "sonar.tests": "./src/tests",
-      "sonar.test.inclusions": "./src/tests**/*.test.jsx,./src/tests/**/*.test.js",
       "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
       "sonar.testExecutionReportPaths": "./test-report.xml",
+      "sonar.qualitygate.wait" : "true"
     },
   },
   () => {},
