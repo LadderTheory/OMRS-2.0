9465694a4ca7360353259b90f412fe1951a074da
Jake Peterson <jacob.peterson@afcent.af.mil>
2020-12-30 09:43:43 -0500

    Adds connection info for azure sonarqube

--- sonar-project.js
+++ sonar-project.js
@@ -1,10 +1,12 @@
 const sonarqubeScanner = require("sonarqube-scanner");
+//Connection Info for SST Lab SonarQube instance. Comment either this one or the Azure one out depending on where you want to send the test results.
 sonarqubeScanner(
   {
-    serverUrl: "https://sonarqube.sst.k8s.afcentcloud.us",
-    token: "8f7fda77a94b7b1c565eed9bbe6156cd7303d450",
+    serverUrl: "http://192.168.1.78:31264",
+    token: "d7fd8bcbe8fb344280999a6af10489c02e43c391",
     options: {
-      "sonar.projectKey": "omrs-master",
+      "sonar.projectKey": "omrs-prototype",
+      "sonar.projectName": "omrs-prototype",
       "sonar.sources": ".",
       "sonar.exclusions": "**/tests/**",
       "sonar.tests": "./frontend/src/tests, ./server/tests",
@@ -16,3 +18,23 @@ sonarqubeScanner(
   },
   () => {},
 );
+
+//Connection Info for Azure SonarQube instance. Comment either this one or the SST Lab one out depending on where you want to send the test results.
+// sonarqubeScanner(
+//   {
+//     serverUrl: "https://sonarqube.sst.k8s.afcentcloud.us",
+//     token: "b42591ab2fb121fa8906861dac61beba9827c4dd",
+//     options: {
+//       "sonar.projectKey": "omrs-master",
+//       "sonar.projectName": "omrs-master",
+//       "sonar.sources": ".",
+//       "sonar.exclusions": "**/tests/**",
+//       "sonar.tests": "./frontend/src/tests, ./server/tests",
+//       "sonar.test.inclusions": "./frontend/src/tests/*.test.js, ./server/tests/*.test.js",
+//       "sonar.javascript.lcov.reportPaths": "frontend/coverage/lcov.info, coverage/lcov.info",
+//       "sonar.testExecutionReportPaths": "./frontend/test-report.xml, ./test-report.xml",
+//       "sonar.qualitygate.wait": "true"
+//     },
+//   },
+//   () => {},
+// );
