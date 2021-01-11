4e6f1b909a047edf690c3a117f758a5cb6876dd6
Jake Peterson <jacob.peterson@afcent.af.mil>
2021-01-11 15:55:11 -0500

    Trying to fix failed pipeline after popper install

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'popper.js/dist/umd/popper.min.js';
+import '@popperjs/core/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import React from "react";
 import ReactDOM from "react-dom";
