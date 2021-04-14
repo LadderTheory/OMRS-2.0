5a1677e6ac95c0ef2e0f83ed66977959b94c950d
Ladder Theory <brenton.ferullo@me.com>
2021-04-14 10:15:22 -0400

    set the default values for 'dateStart' and 'dateEnd' in DateType.js to be 2 days ago

--- frontend/src/Components/ReportFilterTypes/DateType.js
+++ frontend/src/Components/ReportFilterTypes/DateType.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 //Child component for Mission reports component
 function DateType(props)
 {   
+    let two_back = new Date();
+    two_back.setDate(two_back.getDate() - 2);
+
+    const [dateStart, setdateStart] = useState(two_back);
+    const [dateEnd, setDateEnd] = useState(two_back);
+
+    const dateFromValue = (value) => {
+        let d = value.split('-');
+        let newDate = new Date();
+        newDate.setFullYear(d[0]);
+        newDate.setMonth(d[1] - 1);
+        newDate.setDate(d[2]);
+
+        return newDate;
+    }
+
     //Passes the start date back to the parent component
     const dateStartChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         props.handleChangeDateStart(name, value);
+
+        setdateStart(dateFromValue(value))
     }
     //Passes the end date back to the parent component
     const dateEndChange = (e) =>{
         const name = e.target.name;
         const value = e.target.value;
         props.handleChangeDateEnd(name, value);
+
+        setDateEnd(dateFromValue(value));
     }
 
+    const formatDate = (date) => {
+        let year = date.getFullYear();
+        let month = ('0' + (date.getMonth() + 1)).slice(-2);
+        let day = ('0' + (date.getDate())).slice(-2);
+        return `${year}-${month}-${day}`
+    }
+
+    console.log(dateStart)
     return(
         <div className='form-group'>
         <label htmlFor='dateStart' >Starting Date: </label>
-        <input type="date" className="form-control mb-1" id="dateStart" name='dateStart' onChange={dateStartChange} required></input>
+        <input value={formatDate(dateStart)} type="date" className="form-control mb-1" id="dateStart" name='dateStart' onChange={dateStartChange} required></input>
 
         <label htmlFor='dateEnd'>Ending Date: </label>
-        <input type="date" className="form-control mb-1" id="dateEnd" name='dateEnd' onChange={dateEndChange} required></input>
+        <input value={formatDate(dateEnd)} type="date" className="form-control mb-1" id="dateEnd" name='dateEnd' onChange={dateEndChange} required></input>
         </div>
     )
 }
